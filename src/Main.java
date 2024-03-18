96a1a1b7ee97b94eab0b580dcc28275efb7acbcb
Helin <155573055+helinsygl@users.noreply.github.com>
2024-03-19 01:24:33 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-System.out.println("Hello world!");
+System.out.println("Hello world! helin");
 
     }
 }
